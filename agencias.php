a46cd63a9d4495b363fec00ab35c644fa69912cb
sherivan <sherivan07@gmail.com>
2014-04-14 00:40:55 -0500

    Create agencias.php

---
+++ agencias.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<title>Honorable Ayuntamiento Municipal de Pinotepa Nacional</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="../css/estilos.css">
+	<meta name="viewport" content="width=device-width,initial-scale=1"/>
+	<link rel="author" type="text/plain" href="../humans.txt"/>
+	<link rel="sitemap" type="application/xml" title="Sitemap" href="../sitemap.xml"/>
+	<!--[if lt IE9]>
+		<script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+</head>
+<body>
+	<header>
+		<h1 class="fade"><a href="../index.php"><img src="../img/logo.png" alt="Honorable Ayuntamiento de Pinotepa Nacional Oaxaca" /></a></h1>
+	<nav>
+			<ul>
+	   		<li><a href='#' id="pull"><span>↓</span>Gobierno</a>
+	      		<ul>
+	         		<li><a href='#'>Semblanza del Presidente</a></li>
+	            	<li><a href='#'>Mensaje del Presidente</a></li>
+	               	<li><a href='#'>Cabildo</a></li>
+	               	<li class="ultimo"><a href='#'>Comisiones Municipales</a></li>
+	      		</ul>
+	   		</li>
+	   		<li><a href='#'>Leyes y Reglamentos</a>
+	   		<li><a href='#'>Trámites y Servicios</a></li>
+	   		<li><a href='#'>Transparencia</a></li>
+	   		<li><a href='vistas/agencias.php'>Agencias</a></li>
+		</ul>
+	</nav>		
+	</header>
+
+	<section id="contenedor">
+		<section id="agencias">
+			<h2>Agencias de Pinotepa Nacional</h2>
+			<div class="div-agencia">
+				<article>
+					<div><b>Agencia: </b>Corralero</div>
+					<div><b>Agente: </b>Bruno Diaz</div>
+					<img src="../img/user.jpg" class="redonda fade" title="Agente Corralero">
+					<div><b>Dirección:</b> Corralero,Pinotepa Nacional,Oax.</div>
+					<small><a href="https://maps.google.com.mx/maps?q=16.339094,-98.050002&amp;num=1&amp;hl=es-419&amp;ie=UTF8&amp;ll=16.339231,-98.050044&amp;spn=0.001598,0.002642&amp;t=m&amp;z=14&amp;source=embed" style="color:#0000FF;text-align:left;font-size:1em;" target="_blank">Ver mapa de Corralero</a></small>
+					<div><b>Teléfono: </b>1234567890</div>
+					<div><b>Colindancia:</b> Al Poniente el Oceano Pacifico,al Norte Pinotepa Nacional</div>
+				</article>
+				<article>
+					<div><b>Agencia: </b>Corralero</div>
+					<div><b>Agente: </b>Bruno Diaz</div>
+					<img src="../img/user.jpg" class="redonda fade" title="Agente Corralero">
+					<div><b>Dirección:</b> Corralero,Pinotepa Nacional,Oax.</div>
+					<small><a href="https://maps.google.com.mx/maps?q=16.339094,-98.050002&amp;num=1&amp;hl=es-419&amp;ie=UTF8&amp;ll=16.339231,-98.050044&amp;spn=0.001598,0.002642&amp;t=m&amp;z=14&amp;source=embed" style="color:#0000FF;text-align:left;font-size:1em;" target="_blank">Ver mapa de Corralero</a></small>
+					<div><b>Teléfono: </b>1234567890</div>
+					<div><b>Colindancia:</b> Al Poniente el Oceano Pacifico,al Norte Pinotepa Nacional</div>
+				</article>
+				<article>
+					<div><b>Agencia: </b>Corralero</div>
+					<div><b>Agente: </b>Bruno Diaz</div>
+					<img src="../img/user.jpg" class="redonda fade" title="Agente Corralero">
+					<div><b>Dirección:</b> Corralero,Pinotepa Nacional,Oax.</div>
+					<small><a href="https://maps.google.com.mx/maps?q=16.339094,-98.050002&amp;num=1&amp;hl=es-419&amp;ie=UTF8&amp;ll=16.339231,-98.050044&amp;spn=0.001598,0.002642&amp;t=m&amp;z=14&amp;source=embed" style="color:#0000FF;text-align:left;font-size:1em;" target="_blank">Ver mapa de Corralero</a></small>
+					<div><b>Teléfono: </b>1234567890</div>
+					<div><b>Colindancia:</b> Al Poniente el Oceano Pacifico,al Norte Pinotepa Nacional</div>
+				</article>
+				 <article>
+					<div><b>Agencia: </b>Corralero</div>
+					<div><b>Agente: </b>Bruno Diaz</div>
+					<img src="../img/user.jpg" class="redonda fade" title="Agente Corralero">
+					<div><b>Dirección:</b> Corralero,Pinotepa Nacional,Oax.</div>
+					<small><a href="https://maps.google.com.mx/maps?q=16.339094,-98.050002&amp;num=1&amp;hl=es-419&amp;ie=UTF8&amp;ll=16.339231,-98.050044&amp;spn=0.001598,0.002642&amp;t=m&amp;z=14&amp;source=embed" style="color:#0000FF;text-align:left;font-size:1em;" target="_blank">Ver mapa de Corralero</a></small>
+					<div><b>Teléfono: </b>1234567890</div>
+					<div><b>Colindancia:</b> Al Poniente el Oceano Pacifico,al Norte Pinotepa Nacional</div>
+				</article>
+			</div>
+
+			<div class="div-agencia">
+				<article>
+					<div><b>Agencia: </b>Corralero</div>
+					<div><b>Agente: </b>Bruno Diaz</div>
+					<img src="../img/user.jpg" class="redonda fade" title="Agente Corralero">
+					<div><b>Dirección:</b> Corralero,Pinotepa Nacional,Oax.</div>
+					<small><a href="https://maps.google.com.mx/maps?q=16.339094,-98.050002&amp;num=1&amp;hl=es-419&amp;ie=UTF8&amp;ll=16.339231,-98.050044&amp;spn=0.001598,0.002642&amp;t=m&amp;z=14&amp;source=embed" style="color:#0000FF;text-align:left;font-size:1em;" target="_blank">Ver mapa de Corralero</a></small>
+					<div><b>Teléfono: </b>1234567890</div>
+					<div><b>Colindancia:</b> Al Poniente el Oceano Pacifico,al Norte Pinotepa Nacional</div>
+				</article>
+			<article>
+					<div><b>Agencia: </b>Corralero</div>
+					<div><b>Agente: </b>Bruno Diaz</div>
+					<img src="../img/user.jpg" class="redonda fade" title="Agente Corralero">
+					<div><b>Dirección:</b> Corralero,Pinotepa Nacional,Oax.</div>
+					<small><a href="https://maps.google.com.mx/maps?q=16.339094,-98.050002&amp;num=1&amp;hl=es-419&amp;ie=UTF8&amp;ll=16.339231,-98.050044&amp;spn=0.001598,0.002642&amp;t=m&amp;z=14&amp;source=embed" style="color:#0000FF;text-align:left;font-size:1em;" target="_blank">Ver mapa de Corralero</a></small>
+					<div><b>Teléfono: </b>1234567890</div>
+					<div><b>Colindancia:</b> Al Poniente el Oceano Pacifico,al Norte Pinotepa Nacional</div>
+				</article>
+				<article>
+					<div><b>Agencia: </b>Corralero</div>
+					<div><b>Agente: </b>Bruno Diaz</div>
+					<img src="../img/user.jpg" class="redonda fade" title="Agente Corralero">
+					<div><b>Dirección:</b> Corralero,Pinotepa Nacional,Oax.</div>
+					<small><a href="https://maps.google.com.mx/maps?q=16.339094,-98.050002&amp;num=1&amp;hl=es-419&amp;ie=UTF8&amp;ll=16.339231,-98.050044&amp;spn=0.001598,0.002642&amp;t=m&amp;z=14&amp;source=embed" style="color:#0000FF;text-align:left;font-size:1em;" target="_blank">Ver mapa de Corralero</a></small>
+					<div><b>Teléfono: </b>1234567890</div>
+					<div><b>Colindancia:</b> Al Poniente el Oceano Pacifico,al Norte Pinotepa Nacional</div>
+				</article>
+				 <article>
+					<div><b>Agencia: </b>Corralero</div>
+					<div><b>Agente: </b>Bruno Diaz</div>
+					<img src="../img/user.jpg" class="redonda fade" title="Agente Corralero">
+					<div><b>Dirección:</b> Corralero,Pinotepa Nacional,Oax.</div>
+					<small><a href="https://maps.google.com.mx/maps?q=16.339094,-98.050002&amp;num=1&amp;hl=es-419&amp;ie=UTF8&amp;ll=16.339231,-98.050044&amp;spn=0.001598,0.002642&amp;t=m&amp;z=14&amp;source=embed" style="color:#0000FF;text-align:left;font-size:1em;" target="_blank">Ver mapa de Corralero</a></small>
+					<div><b>Teléfono: </b>1234567890</div>
+					<div><b>Colindancia:</b> Al Poniente el Oceano Pacifico,al Norte Pinotepa Nacional</div>
+				</article>
+			</div>
+			<div class="div-agencia">
+				<article>
+					<div><b>Agencia: </b>Corralero</div>
+					<div><b>Agente: </b>Bruno Diaz</div>
+					<img src="../img/user.jpg" class="redonda fade" title="Agente Corralero">
+					<div><b>Dirección:</b> Corralero,Pinotepa Nacional,Oax.</div>
+					<small><a href="https://maps.google.com.mx/maps?q=16.339094,-98.050002&amp;num=1&amp;hl=es-419&amp;ie=UTF8&amp;ll=16.339231,-98.050044&amp;spn=0.001598,0.002642&amp;t=m&amp;z=14&amp;source=embed" style="color:#0000FF;text-align:left;font-size:1em;" target="_blank">Ver mapa de Corralero</a></small>
+					<div><b>Teléfono: </b>1234567890</div>
+					<div><b>Colindancia:</b> Al Poniente el Oceano Pacifico,al Norte Pinotepa Nacional</div>
+				</article>
+			<article>
+					<div><b>Agencia: </b>Corralero</div>
+					<div><b>Agente: </b>Bruno Diaz</div>
+					<img src="../img/user.jpg" class="redonda fade" title="Agente Corralero">
+					<div><b>Dirección:</b> Corralero,Pinotepa Nacional,Oax.</div>
+					<small><a href="https://maps.google.com.mx/maps?q=16.339094,-98.050002&amp;num=1&amp;hl=es-419&amp;ie=UTF8&amp;ll=16.339231,-98.050044&amp;spn=0.001598,0.002642&amp;t=m&amp;z=14&amp;source=embed" style="color:#0000FF;text-align:left;font-size:1em;" target="_blank">Ver mapa de Corralero</a></small>
+					<div><b>Teléfono: </b>1234567890</div>
+					<div><b>Colindancia:</b> Al Poniente el Oceano Pacifico,al Norte Pinotepa Nacional</div>
+				</article>
+				<article>
+					<div><b>Agencia: </b>Corralero</div>
+					<div><b>Agente: </b>Bruno Diaz</div>
+					<img src="../img/user.jpg" class="redonda fade" title="Agente Corralero">
+					<div><b>Dirección:</b> Corralero,Pinotepa Nacional,Oax.</div>
+					<small><a href="https://maps.google.com.mx/maps?q=16.339094,-98.050002&amp;num=1&amp;hl=es-419&amp;ie=UTF8&amp;ll=16.339231,-98.050044&amp;spn=0.001598,0.002642&amp;t=m&amp;z=14&amp;source=embed" style="color:#0000FF;text-align:left;font-size:1em;" target="_blank">Ver mapa de Corralero</a></small>
+					<div><b>Teléfono: </b>1234567890</div>
+					<div><b>Colindancia:</b> Al Poniente el Oceano Pacifico,al Norte Pinotepa Nacional</div>
+				</article>
+				 <article>
+					<div><b>Agencia: </b>Corralero</div>
+					<div><b>Agente: </b>Bruno Diaz</div>
+					<img src="../img/user.jpg" class="redonda fade" title="Agente Corralero">
+					<div><b>Dirección:</b> Corralero,Pinotepa Nacional,Oax.</div>
+					<small><a href="https://maps.google.com.mx/maps?q=16.339094,-98.050002&amp;num=1&amp;hl=es-419&amp;ie=UTF8&amp;ll=16.339231,-98.050044&amp;spn=0.001598,0.002642&amp;t=m&amp;z=14&amp;source=embed" style="color:#0000FF;text-align:left;font-size:1em;" target="_blank">Ver mapa de Corralero</a></small>
+					<div><b>Teléfono: </b>1234567890</div>
+					<div><b>Colindancia:</b> Al Poniente el Oceano Pacifico,al Norte Pinotepa Nacional</div>
+				</article>
+			</div>
+		</section>
+	</section>
+		<footer>
+		<div class="footer">
+			NÚMEROS DE EMERGENCIA
+			066 Emergencias				4820299 Protección Civíl
+			065 Cruz Roja				089 Denuncia Ciudadana
+			4844122 Bomberos			089 Denuncia Ciudadana
+			4844122 Seguridad Pública	071 CFE
+		</div>
+		<div class="footer">
+			©2014-2016 H. Ayuntamiento de Pinotepa Nacional. <br/>
+			Portal Oficial del Gobierno Municipal de Pinotepa Nacional.
+
+		</div>
+	</footer>
+</body>
+</html>
